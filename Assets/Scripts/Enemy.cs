9a044deee33fbfdeb7428bbc0a4944334e9978f2
TimTheGr8 <tjanssen4@gmail.com>
2023-04-17 20:52:05 -0500

    Added enemy movement.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 4f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,14 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // move left at 4 meters per second
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        // if left side of screen 
+        // respawn on right side at random y location
+        if(transform.position.x <= -9)
+        {
+            transform.position = new Vector3(11, Random.Range(-3f, 5.5f), 0);
+        }
     }
 }
