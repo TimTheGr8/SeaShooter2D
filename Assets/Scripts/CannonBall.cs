65cb9edae1466dfc3f51a60717eaa9af3d290bde
TimTheGr8 <tjanssen4@gmail.com>
2023-05-08 17:49:01 -0500

    Added the cannonball splash functionality so the cannoballs do not fly forever.

--- Assets/Scripts/CannonBall.cs
+++ Assets/Scripts/CannonBall.cs
@@ -6,17 +6,41 @@ public class CannonBall : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 5;
+    [SerializeField]
+    private float _shotTimer = 0.5f;
+    [SerializeField]
+    private GameObject _waterSplash;
+    [SerializeField]
+    private float _splashTime = 0.2f;
 
+    private void Start()
+    {
+        StartCoroutine(DestroyCountdown());
+    }
 
     void Update()
     {
         transform.Translate(Vector3.up * _speed * Time.deltaTime);
         if (transform.position.x >= 11)
         {
-            if (transform.parent.name == "Triple_Shot(Clone)")
-                Destroy(transform.parent.gameObject);
-            else
-                Destroy(gameObject);
+            DestroyCannonball();
         }
+    }
+
+    private void DestroyCannonball()
+    {
+        GameObject splash = Instantiate(_waterSplash, transform.position, Quaternion.identity);
+        Destroy(splash, _splashTime);
+
+        if (transform.parent.name == "Triple_Shot(Clone)")
+            Destroy(transform.parent.gameObject);
+        else
+            Destroy(gameObject);
+    }
+
+    IEnumerator DestroyCountdown ()
+    {
+        yield return new WaitForSeconds(_shotTimer);
+        DestroyCannonball();
     }
 }
